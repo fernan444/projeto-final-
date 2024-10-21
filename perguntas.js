4b26b63f035cb03e4382faa0585e018558c27728
fernan444 <165675556+fernan444@users.noreply.github.com>
2024-10-21 12:01:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'historia',
+    'qual a guerra mais longa da historia?',
+    'a guerra de reconquista'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'histororia',
+    'quanto tempo durou a guerra dos sem anos?',
+    'apear do nome ela durou 12 ano!!!'
+)
+
+criaCartao(
+    'biologia',
+    'qual a dierença entre chire e corno?',
+    'chifres sao temporarios, e cornos sao permanentes'
+)
